e0f7e36ce0a05127925b3a9f205a5f03233a6c7b
Emil Hultman <mokoma2002@gmail.com>
2021-04-12 05:03:35 +0300

    Muutoksia
    -Logomuutoksia, jäsenteltyä & tekstiä
    -THL api paskaa tehty
    -KRIP AUTA

---
+++ koodi.js
@@ -0,0 +1,17 @@
+async function getData() {
+    return await fetch('https://sampo.thl.fi/pivot/prod/fi/epirapo/covid19case/fact_epirapo_covid19case.json?row=hcdmunicipality2020-445196.&column=dateweek20200101-509030&filter=measure-444833', {mode: "no-cors"})
+      .then(r => r.json())
+      .then(data => data);
+  }
+  async function main() {
+    console.log(await getData());
+  }
+
+
+  
+  async function main() {
+    data = await getData();
+    console.log(data.infected)
+  }
+  
+main()
